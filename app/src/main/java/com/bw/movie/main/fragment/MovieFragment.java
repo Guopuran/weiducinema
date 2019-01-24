47c9ea648dd556f159ccf2fc5ad3db16701237a0
liuguangyan <2451528553@qq.com>
2019-01-24 21:01:47 +0800

    2

---
+++ app/src/main/java/com/bw/movie/main/fragment/MovieFragment.java
@@ -0,0 +1,28 @@
+package com.bw.movie.main.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bw.movie.R;
+
+
+public class MovieFragment extends Fragment {
+
+
+    public MovieFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_movie, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/bw/movie/main/fragment/MyFragment.java
@@ -0,0 +1,4 @@
+package com.bw.movie.main.fragment;
+
+public class MyFragment {
+}
