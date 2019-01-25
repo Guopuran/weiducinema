a17f1d15f350c35b8b381e913b838ffb1c9d4bcf
liuguangyan <2451528553@qq.com>
2019-01-25 11:27:28 +0800

    5

---
+++ app/src/main/java/com/bw/movie/util/RegularUtil.java
@@ -0,0 +1,33 @@
+package com.bw.movie.util;
+
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @详情 正则判断
+ *
+ * @创建日期 2018/12/29 20:07
+ *
+ */
+public class RegularUtil {
+    public static final String REG_MOBILE = "^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$";
+
+    //判断手机号
+    public static boolean isPhone(String mphone)
+
+
+
+    {
+        return Pattern.matches(REG_MOBILE,mphone);
+    }
+
+    //判断是否为空
+    public static boolean isNull(String mnull){
+        return mnull.equals("");
+    }
+
+    //判断密码的长度
+    public static boolean isPass(String mpass){
+        return mpass.length()<6;
+    }
+}
