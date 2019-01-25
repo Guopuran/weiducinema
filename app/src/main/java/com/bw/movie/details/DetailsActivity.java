b2e5ca1799814c021b4810f1d140681919609c29
guopuran <www.1174965274@qq.com>
2019-01-25 14:55:44 +0800

    33

---
+++ app/src/main/java/com/bw/movie/details/DetailsActivity.java
@@ -0,0 +1,34 @@
+package com.bw.movie.details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bw.movie.R;
+import com.bw.movie.base.BaseActivity;
+
+public class DetailsActivity extends BaseActivity {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void success(Object object) {
+
+    }
+
+    @Override
+    protected void failed(String error) {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_details;
+    }
+}
--- app/src/main/java/com/bw/movie/main/fragment/CinemaFragment.java
+++ app/src/main/java/com/bw/movie/main/fragment/CinemaFragment.java
@@ -1,4 +1,31 @@
 package com.bw.movie.main.fragment;
 
-public class CinemaFragment {
+import com.bw.movie.R;
+import com.bw.movie.base.BaseFragment;
+
+public class CinemaFragment extends BaseFragment {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void success(Object object) {
+
+    }
+
+    @Override
+    protected void failed(String error) {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_cinema;
+    }
 }
