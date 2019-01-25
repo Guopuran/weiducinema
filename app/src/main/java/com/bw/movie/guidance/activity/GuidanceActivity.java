11e31db5733ec6013dd758b715789d59793402c0
liuguangyan <2451528553@qq.com>
2019-01-25 08:23:45 +0800

    22

--- app/src/main/java/com/bw/movie/guidance/activity/GuidanceActivity.java
+++ app/src/main/java/com/bw/movie/guidance/activity/GuidanceActivity.java
@@ -1,5 +1,7 @@
 package com.bw.movie.guidance.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -13,6 +15,7 @@ import com.bw.movie.guidance.fragment.GuidanceFirstFragment;
 import com.bw.movie.guidance.fragment.GuidanceFourthFragment;
 import com.bw.movie.guidance.fragment.GuidanceSencondFragment;
 import com.bw.movie.guidance.fragment.GuidanceThirdFragment;
+import com.bw.movie.login.LoginActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +30,8 @@ public class GuidanceActivity extends BaseActivity {
      @BindView(R.id.guide_group)
      RadioGroup group;
      private List<Fragment> mList;
+     private SharedPreferences sharedPreferences;
+     private SharedPreferences.Editor editor;
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_guidance;
@@ -45,6 +50,8 @@ public class GuidanceActivity extends BaseActivity {
            mList.add(new GuidanceSencondFragment());
            mList.add(new GuidanceThirdFragment());
            mList.add(new GuidanceFourthFragment());
+           sharedPreferences = getSharedPreferences("User",MODE_PRIVATE);
+           editor = sharedPreferences.edit();
            viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
                @Override
                public Fragment getItem(int i) {
@@ -56,6 +63,71 @@ public class GuidanceActivity extends BaseActivity {
                    return mList.size();
                }
            });
+           //点击改变页面
+      /*  group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radio_fir:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.radio_sen:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.radio_thi:
+                        viewPager.setCurrentItem(2);
+                        break;
+                    case R.id.radio_fou:
+                        viewPager.setCurrentItem(3);
+                        break;
+                        default:
+                            break;
+                }
+            }
+        });*/
+        //滑动页面切换按钮
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+                switch (i){
+                    case 0:
+                        group.check(R.id.radio_fir);
+                        break;
+                    case 1:
+                        group.check(R.id.radio_sen);
+                        break;
+                    case 2:
+                        group.check(R.id.radio_thi);
+                        break;
+                    case 3:
+                        editor.putInt("falg",1);
+                        editor.commit();
+                        Intent intent = new Intent(GuidanceActivity.this,LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                        group.check(R.id.radio_fou);
+                        break;
+                        default:
+                            break;
+                }
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+        //判断是否是第一次进入
+        int falg = sharedPreferences.getInt("falg", 0);
+        if (falg>0){
+            Intent intent = new Intent(GuidanceActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
