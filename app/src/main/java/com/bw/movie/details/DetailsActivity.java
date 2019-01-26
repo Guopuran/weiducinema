d2bc6b3fcfc8584fac358f399a9184c7b4b0fffb
guopuran <www.1174965274@qq.com>
2019-01-26 18:34:45 +0800

    33

--- app/src/main/java/com/bw/movie/details/DetailsActivity.java
+++ app/src/main/java/com/bw/movie/details/DetailsActivity.java
@@ -1,7 +1,15 @@
 package com.bw.movie.details;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -10,7 +18,10 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bw.movie.R;
 import com.bw.movie.base.BaseActivity;
+import com.bw.movie.base.BaseFullBottomSheetFragment;
+import com.bw.movie.custom.CustomDialog;
 import com.bw.movie.details.bean.DetailsMovieBean;
+import com.bw.movie.details.fragment.DetailsFragment;
 import com.bw.movie.util.Apis;
 
 import butterknife.BindView;
@@ -21,7 +32,7 @@ import butterknife.OnClick;
  * @描述 影片的详情
  * @创建日期 2019/1/25 16:52
  */
-public class DetailsActivity extends BaseActivity {
+public class DetailsActivity extends BaseActivity{
     @BindView(R.id.details_iamge_back_top)
     ImageView back_image;
     @BindView(R.id.details_movie_button_attention)
@@ -42,9 +53,20 @@ public class DetailsActivity extends BaseActivity {
     Button button_back;
     @BindView(R.id.details_movie_button_buy)
     Button button_buy;
+    private int screenHeight;
+    private int dialog_height;
 
     @Override
     protected void initData() {
+        back_image.setAlpha(0.2f);
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;         // 屏幕宽度（像素）
+        int height = dm.heightPixels;       // 屏幕高度（像素）
+        dialog_height = height-getResources().getDimensionPixelOffset(R.dimen.dp_110);
+
+
         //请求详情
         initDetailUrl();
     }
@@ -61,6 +83,7 @@ public class DetailsActivity extends BaseActivity {
            DetailsMovieBean.ResultBean result = detailsMovieBean.getResult();
            Glide.with(this).load(result.getImageUrl()).into(image_big);
            Glide.with(this).load(result.getImageUrl()).into(back_image);
+           back_image.setScaleType(ImageView.ScaleType.FIT_XY);
            //设置电影mingc
            text_name.setText(result.getName());
            //设置是否被关注
@@ -97,9 +120,12 @@ public class DetailsActivity extends BaseActivity {
                 break;
             //影片详情
             case R.id.details_lin_movie_text_details:
+                View _details = initDiaLog(R.layout.frag_details);
+
                 break;
             //影片预告
             case R.id.details_lin_movie_text_notice:
+
                 break;
             //影片剧照
             case R.id.details_lin_movie_text_photo:
@@ -115,5 +141,18 @@ public class DetailsActivity extends BaseActivity {
                 break;
             default:break;
         }
+    }
+    public View initDiaLog(int getLayoutId){
+        View details_view=View.inflate(this,getLayoutId,null);
+        final CustomDialog dialog=new CustomDialog(this,details_view,false,true,dialog_height);
+        dialog.show();
+        ImageView image_back=details_view.findViewById(R.id.dialog_back);;
+        image_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        return  details_view;
     }
 }
