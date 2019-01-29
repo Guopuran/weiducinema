f61537e9b81cbcdb800576c742c0719827339339
liuguangyan <2451528553@qq.com>
2019-01-29 17:26:22 +0800

    Merge branch 'master' of https://github.com/Guopuran/weiducinema
    # Conflicts:
    #	app/src/main/java/com/bw/movie/util/Apis.java

---
+++ app/src/main/java/com/bw/movie/main/activity/LocationActivity.java
@@ -0,0 +1,46 @@
+package com.bw.movie.main.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bw.movie.R;
+import com.bw.movie.base.BaseActivity;
+import com.bw.movie.util.ToastUtil;
+import com.zaaach.citypicker.CityPicker;
+import com.zaaach.citypicker.adapter.OnPickListener;
+import com.zaaach.citypicker.model.City;
+import com.zaaach.citypicker.model.HotCity;
+import com.zaaach.citypicker.model.LocatedCity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocationActivity extends BaseActivity {
+
+    @Override
+    protected void initData()
+    {
+
+
+    }
+
+    @Override
+    protected void success(Object object) {
+
+    }
+
+    @Override
+    protected void failed(String error) {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+}
